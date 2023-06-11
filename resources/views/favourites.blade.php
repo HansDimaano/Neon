38c29dbc1a57ef2a5574a7510c15bee0f127b5b5
Hans Dimaano <71628557+HansDimaano@users.noreply.github.com>
2023-06-11 15:29:55 +0400

    Found the bug where I pasted the wrong code

--- resources/views/favourites.blade.php
+++ resources/views/favourites.blade.php
@@ -1,152 +1,216 @@
-<!DOCTYPE html>
-<html lang="en">
+@extends('layouts.user_panel')
 
-<head>
-    <title>{{ $user->name }}</title>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
-    <link rel="stylesheet" href="{{ asset('css/ecard.css') }}">
-    <link rel="stylesheet"
-        href="https://cdnjs.cloudflare.com/ajax/libs/MaterialDesign-Webfont/3.6.95/css/materialdesignicons.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css"
-        integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
+@section('content')
+    @php
+    $card_list = Auth::user()->card;
+    $favourites = Auth::user()
+        ->favourites()
+        ->orderBy('created_at', 'DESC')
+        ->skip($page * $take)
+        ->take($take)
+        ->get();
 
-</head>
+    @endphp
+    <style>
+        .profile-image-box {
+            display: block;
+            width: 100px;
+            height: 118px;
+            object-fit: cover;
+        }
 
-<body>
-    <div class="page-content page-container" id="page-content">
-        <div class="padding">
-            <div class="row center justify-content-center">
-                <div class="col-xl-8 col-lg-10 col-md-12">
-                    <div class="card user-card-full">
-                        <div class="row m-l-0 m-r-0">
-                            <div class="col-sm-12 bg-c-lite-green user-profile">
+        .card {
+            width: 500px;
+            min-width: 300;
+            border: none;
+            border-radius: 10px;
+            background-color: #fff
+        }
 
-                                <div class="card-block text-center text-white">
-                                    <div class="m-b-25"> <img src="https://img.icons8.com/bubbles/100/000000/user.png"
-                                            class="img-radius" alt="User-Profile-Image"> </div>
-                                    <h6 class="f-w-600">{{ $user->fname . ' ' . $user->lname }}</h6>
-                                    <p>Web Designer</p>
+        .card-holder {
+            padding-left: 6px;
+        }
 
-                                    <!-- Edit button for authenticated user -->
-                                    @if (Auth::check() && Auth::user()->id == $user->id)
-                                        <a href="{{ route('home') }}" style="color: white; padding-right:2.5rem">
-                                            <i class="far fa-edit fa-lg"></i>
-                                        </a>
-                                    @endif
+        @media only screen and (max-width: 600px) {
+            .card {
+                width: 100%;
+                border: none;
+                border-radius: 10px;
 
-                                    <!-- Favorite button -->
-                                    <a @guest @php
-                                        Session::put('url.intended', URL::full());
-                                    @endphp href="{{ route('login') }}" @else id="favBtn"
-                                    @endguest style="color: white">
-                                    <i class="far fa-heart fa-lg"></i>
-                                </a>
-                            </div>
-                        </div>
-                        <div class="col-sm-8">
-                            <div class="card-block">
-                                <h6 class="m-b-20 p-b-5 b-b-default f-w-600">Information</h6>
-                                <div class="row">
-                                    <div class="col-sm-6">
-                                        <p class="m-b-10 f-w-600">Email</p>
-                                        <h6 class="text-muted f-w-400">{{ $user->email }}</h6>
+            }
+
+            .card-holder {
+                padding: 0px;
+            }
+        }
+
+        .stats {
+            background: #f2f5f8 !important;
+            color: #000 !important
+        }
+
+        .articles {
+            font-size: 10px;
+            color: #a1aab9
+        }
+
+        .number1 {
+            font-weight: 500
+        }
+
+        .followers {
+            font-size: 10px;
+            color: #a1aab9
+        }
+
+        .number2 {
+            font-weight: 500
+        }
+
+        .rating {
+            font-size: 10px;
+            color: #a1aab9
+        }
+
+        .number3 {
+            font-weight: 500
+        }
+
+        .tooltip {
+            position: relative;
+            display: inline-block;
+        }
+
+        .tooltip .tooltiptext {
+            visibility: hidden;
+            width: 140px;
+            background-color: #555;
+            color: #fff;
+            text-align: center;
+            border-radius: 6px;
+            padding: 5px;
+            position: absolute;
+            z-index: 1;
+            bottom: 150%;
+            left: 50%;
+            margin-left: -75px;
+            opacity: 0;
+            transition: opacity 0.3s;
+        }
+
+        .tooltip .tooltiptext::after {
+            content: "";
+            position: absolute;
+            top: 100%;
+            left: 50%;
+            margin-left: -5px;
+            border-width: 5px;
+            border-style: solid;
+            border-color: #555 transparent transparent transparent;
+        }
+
+        .tooltip:hover .tooltiptext {
+            visibility: visible;
+            opacity: 1;
+        }
+
+    </style>
+    <div id="content" class="content">
+        <div class="container-fluid">
+
+            <div class="row">
+                <div class="col-7">
+                    <strong class="card-title">Favourites</strong>
+                </div>
+                <div class="col-1">
+
+                </div>
+
+                <div class="col-1">
+                    @if ($page > 0)
+                        <a href="{{ route('user.profile.favourite', $page - 1) }}"> <i
+                                class="fa fa-lg fa-arrow-left"></i> </a>
+                    @endif
+                </div>
+                <div class="col-1">
+                    @if (($page + 1) * $take < Auth::user()->favourites()->count())
+                        <a href="{{ route('user.profile.favourite', $page + 1) }}"> <i
+                                class="fa fa-lg fa-arrow-right"></i> </a>
+                    @endif
+                </div>
+            </div>
+
+            <div class="row mt-3">
+
+                @foreach ($favourites as $item_f)
+                    @php
+                        $ratings = $item_f->card->user->ratings;
+                        $rating = $ratings->avg('rating');
+                        $rating = $rating == null ? 0 : $rating;
+                    @endphp
+                    <div class="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-4 card-holder">
+                        <div class="col card p-3">
+                            <div class="d-flex align-items-center">
+                                <div class="img-fluid"> <img style="max-width: unset"
+                                        src="{{ asset('uploads/user/' . ($item_f->card->user->profilePicture == null ? 'avatar.png' : $item_f->card->user->profilePicture->name)) }}"
+                                        class="rounded profile-image-box"> </div>
+                                <div class="ml-3 w-100">
+                                    <h4 class="mb-0 mt-0">
+                                        <strong>{{ $item_f->card->user->fname . ' ' . $item_f->card->user->lname }}</strong>
+                                    </h4> <span>{{ '@' . $item_f->card->user->username }}</span>
+                                    <div
+                                        class="p-2 mt-2 bg-primary d-flex justify-content-between rounded text-white stats">
+                                        <div class="d-flex flex-column"> <span class="articles">Views</span> <span
+                                                class="number1">{{ App\Http\Traits\NumberTrait::number_format_short($item_f->card->user->views) }}</span>
+                                        </div>
+                                        <div class="d-flex flex-column"> <span class="followers">Followers</span> <span
+                                                class="number2">{{ App\Http\Traits\NumberTrait::number_format_short($item_f->card->user->followers()->count()) }}</span>
+                                        </div>
+                                        <div class="d-flex flex-column"> <span class="rating">Rating</span> <span
+                                                class="number3">{{ $rating }}</span> </div>
                                     </div>
-                                    <div class="col-sm-6">
-                                        <p class="m-b-10 f-w-600">Phone</p>
-                                        <h6 class="text-muted f-w-400"><a
-                                                href="tel:{{ $user->phone }}">{{ $user->phone }}</a></h6>
-                                    </div>
+                                    <div class="button mt-2 d-flex flex-row align-items-center"> <button
+                                            class="btn btn-sm btn-outline-primary w-100" disabled>{{$item_f->card->card_name}}</button> <a
+                                            href="{{ route('user.profile', $item_f->card->user->id) }}"
+                                            class="btn btn-sm btn-primary w-100 ml-2">Profile</a> </div>
                                 </div>
-                                <h6 class="m-b-20 m-t-40 p-b-5 b-b-default f-w-600">Impressions</h6>
-                                <div class="row">
-                                    <div class="col-sm-6">
-                                        <p class="m-b-10 f-w-600">Rating</p>
-                                        <h6 class="text-muted f-w-400">
-                                            {{ $user->rate_counter == 0 ? 0 : round($user->total_rating / $user->rate_counter, 1) }}/10
-                                        </h6>
-                                    </div>
-                                    <div class="col-sm-6">
-                                        <p class="m-b-10 f-w-600">Profile Views</p>
-                                        <h6 class="text-muted f-w-400">
-                                            {{ str_pad($user->views, 8, '0', STR_PAD_LEFT) }}</h6>
-                                    </div>
-                                </div>
-                                <div class="row">
-                                    <div class="col-sm-6">
-                                        <p class="m-b-10 f-w-600">Rating</p>
-                                        <h6 class="text-muted f-w-400">
-                                            {{ $user->rate_counter == 0 ? 0 : round($user->total_rating / $user->rate_counter, 1) }}/10
-                                        </h6>
-                                    </div>
-                                    <div class="col-sm-6">
-                                        <p class="m-b-10 f-w-600">Profile Views</p>
-                                        <h6 class="text-muted f-w-400">
-                                            {{ str_pad($user->views, 8, '0', STR_PAD_LEFT) }}</h6>
-                                    </div>
-                                </div>
-                                <ul class="social-link list-unstyled m-t-40 m-b-10">
-                                    <li><a href="#!" data-toggle="tooltip" data-placement="bottom" title=""
-                                            data-original-title="facebook" data-abc="true"><i
-                                                class="mdi mdi-facebook feather icon-facebook facebook"
-                                                aria-hidden="true"></i></a></li>
-                                    <li><a href="#!" data-toggle="tooltip" data-placement="bottom" title=""
-                                            data-original-title="twitter" data-abc="true"><i
-                                                class="mdi mdi-twitter feather icon-twitter twitter"
-                                                aria-hidden="true"></i></a></li>
-                                    <li><a href="#!" data-toggle="tooltip" data-placement="bottom" title=""
-                                            data-original-title="instagram" data-abc="true"><i
-                                                class="mdi mdi-instagram feather icon-instagram instagram"
-                                                aria-hidden="true"></i></a></li>
-                                </ul>
                             </div>
                         </div>
                     </div>
-                </div>
+                @endforeach
+
             </div>
+
         </div>
-    </div>
-</div>
 
-<script>
-    // Log the current URL
-    console.log(window.location.href);
 
-    // Handle favorite button click
-    $("#favBtn").on('click', function() {
-        loadData();
-    });
+        <div id="blank">
+        </div>
 
-    // Load data from the server
-    function loadData() {
-        $.get(
-                window.location.href + "/makefav", {
-                    paramOne: 1,
-                    paramX: 'abc'
-                },
-                function(data, success) {
-                    console.log(data);
-                    console.log(success);
-                    if (data == "1") {
-                        $("#favBtn").css({
-                            "color": "#007bff"
-                        });
-                    } else if (data == "0") {
-                        $("#favBtn").css({
-                            "color": "white"
-                        });
-                    }
-                }
-            )
-            .fail(function(success) {
-                console.log(success.status);
-            });
-    }
-</script>
+    </div><!-- .animated -->
+@endsection
 
-</body>
+@section('js')
+    <script>
+        var window_height = jQuery(window).height();
+        var window_width = jQuery(window).width();
+        var content_height = jQuery('#content').height();
+        setFooterDistance();
+        //setTableWidth();
+        console.log(window_height);
 
-</html>
+        jQuery(window).resize(function() {
+            window_height = jQuery(window).height();
+            window_width = jQuery(window).width();
+            setFooterDistance();
+            //setTableWidth();
+            //jQuery("#table").css('transform', 'scale(2)');
+        });
+
+        function setFooterDistance() {
+            jQuery('#blank').height(window_height - content_height - 200);
+            console.log(window_height);
+            console.log(window_width);
+        }
+    </script>
+@endsection
